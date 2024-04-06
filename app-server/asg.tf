59085359009fec00045c4ba6b6854c5589356e1e
rahulrwt <rahulrawat.rrc@gmail.com>
2024-04-07 00:30:26 +0530

    Fixed ASG issue

--- app-server/asg.tf
+++ app-server/asg.tf
@@ -3,9 +3,10 @@ resource "aws_autoscaling_group" "app-server-asg" {
   launch_template {
     id      = aws_launch_template.app-server-template.id
     version = "$Latest"
+    # security_groups = ["${aws_security_group.cooking-corner-sg.id}"]
   }
   min_size             = 0
   max_size             = 1
   desired_capacity     = 1
-  vpc_zone_identifier  = [local.subnet]
+  vpc_zone_identifier  = [local.subnet] 
 }
--- app-server/launch-template.tf
+++ app-server/launch-template.tf
@@ -3,7 +3,8 @@ resource "aws_launch_template" "app-server-template" {
   image_id      = local.app-server-ami
   instance_type = local.instance_type
   key_name      = local.aws-key-pair-name
-  user_data = base64encode(local.user_data_script)  
+  user_data     = base64encode(local.user_data_script)  
+  
   # latest_version = true
   block_device_mappings {
     device_name = "/dev/sda1"
@@ -12,6 +13,12 @@ resource "aws_launch_template" "app-server-template" {
       volume_type = "gp2"
     }
   }
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = ["${aws_security_group.cooking-corner-sg.id}"]
+  }
+
     tag_specifications {
     resource_type = "instance"
     tags = {
